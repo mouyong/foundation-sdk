4d32ed703e7665f1471d5742f23f6fe719afd5fb
mouyong <my242551325@gmail.com>
2021-02-21 23:44:44 +0800

    feat: 更新 guzzlehttp 版本

--- src/Providers/LogServiceProvider.php
+++ src/Providers/LogServiceProvider.php
@@ -13,7 +13,46 @@ class LogServiceProvider implements ServiceProviderInterface
         $pimple['log'] = function ($pimple) {
             $name = $pimple['options']['log']['name'] ?? 'foundation';
 
+            $logger = null;
+
+            // TODO: 更换 logger 为可自定义
+            Log::setLogger($logger);
+
             return Log::getLogger()->withName($name);
         };
+    }
+
+    public function formatLogConfig($app)
+    {
+        if (!empty($app['config']->get('log.channels'))) {
+            return $app['config']->get('log');
+        }
+
+        if (empty($app['config']->get('log'))) {
+            return [
+                'log' => [
+                    'default' => 'errorlog',
+                    'channels' => [
+                        'errorlog' => [
+                            'driver' => 'errorlog',
+                            'level' => 'debug',
+                        ],
+                    ],
+                ],
+            ];
+        }
+
+        return [
+            'log' => [
+                'default' => 'single',
+                'channels' => [
+                    'single' => [
+                        'driver' => 'single',
+                        'path' => $app['config']->get('log.file') ?: \sys_get_temp_dir().'/logs/easywechat.log',
+                        'level' => $app['config']->get('log.level', 'debug'),
+                    ],
+                ],
+            ],
+        ];
     }
 }
